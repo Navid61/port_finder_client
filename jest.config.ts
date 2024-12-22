01ca2491cb4f9a8eee038c20c245aba1ef98d380
Navid <navidtaheri@gmail.com>
2024-12-22 09:30:00 +0100

    jest config file updated.

--- jest.config.ts
+++ jest.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+     '^@/(.*)$': '<rootDir>/src/$1'
    
   },
   "collectCoverageFrom": [
